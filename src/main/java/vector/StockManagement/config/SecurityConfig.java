8fb1a058d1200551f5381290b81d87cf1936f1ff
vieira-klgwn <ntwavieir7@gmail.com>
2025-09-11 20:30:47 +0200

    added cors port of react

--- src/main/java/vector/StockManagement/config/SecurityConfig.java
+++ src/main/java/vector/StockManagement/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8081"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
