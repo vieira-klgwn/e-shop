1ae540468b0120a51d434a50a72ac49c1c28e8d9
vieira-klgwn <ntwavieir7@gmail.com>
2025-09-14 17:19:14 +0200

    allowed api/users to skip tenant filter

--- src/main/java/vector/StockManagement/config/TenantFilter.java
+++ src/main/java/vector/StockManagement/config/TenantFilter.java
@@ -3,24 +3,30 @@ package vector.StockManagement.config;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import vector.StockManagement.model.User;
 
+
 import java.io.IOException;
 
 @Component
 public class TenantFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(TenantFilter.class);
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String requestURI = httpRequest.getRequestURI();
 
+
         // Skip filter for /api/tenants/admin
-        if ("/api/tenants/admin".equals(requestURI) || requestURI.startsWith("/api/auth/") || requestURI.contains("super_user/login") || requestURI.contains("api/tenants")) {
+        if ("/api/tenants/admin".equals(requestURI) || requestURI.startsWith("/api/auth/") || requestURI.contains("super_user/login") || requestURI.contains("api/tenants") || requestURI.contains("api/users")) {
             chain.doFilter(request, response);
             return;
         }
@@ -28,13 +34,23 @@ public class TenantFilter implements Filter {
         try {
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
+
+            logger.info("Authenticated user: {}", auth);
+            logger.info("Authenticated user: {}", auth.getName());
+            logger.info("Authenticated user: {}", auth.getPrincipal());
+            logger.info("Authenticated user: {}", auth.getDetails());
+            logger.info("Authenticated user: {}", auth.getAuthorities());
+            logger.info("Authenticated user: {}", auth.getCredentials());
+
+
             if (auth != null && auth.getPrincipal() instanceof User user && user.getTenant() != null) {
                 TenantContext.setTenantId(user.getTenant().getId());
+                logger.info("Authenticated user: {}", user.getTenant().getId());
             }
             chain.doFilter(request, response);
         } finally {
 //            TenantContext.clear();
-            System.out.println("Tenant filter executed");
+            System.out.println("Tenant filter eAuthenticationControllerxecuted");
 
         }
     }
--- src/main/java/vector/StockManagement/config/TenantFilterAspect.java
+++ src/main/java/vector/StockManagement/config/TenantFilterAspect.java
@@ -2,13 +2,17 @@ package vector.StockManagement.config;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import vector.StockManagement.auth.AuthenticationController;
 
 @Aspect
 @Component
 public class TenantFilterAspect {
+    private static final Logger logger = LoggerFactory.getLogger(TenantFilterAspect.class);
 
     private final HibernateTenantFilterConfiguration config;
 
@@ -24,7 +28,7 @@ public class TenantFilterAspect {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         if (attributes != null) {
             String requestUri = attributes.getRequest().getRequestURI();
-            if (requestUri.contains("/api/tenants") || requestUri.startsWith("/api/auth/") || requestUri.contains("super_user/login")) {
+            if (requestUri.contains("/api/tenants") || requestUri.startsWith("/api/auth/") || requestUri.contains("super_user/login") || requestUri.contains("/api/users")) {
                 return; // Skip applying the tenant filter
             }
 
