77e771dd9ea0a86eb3db59aab93b911edef34df1
vieira-klgwn <ntwavieir7@gmail.com>
2025-09-14 16:03:08 +0200

    allowed super admin to view all users

--- src/main/java/vector/StockManagement/config/SecurityConfig.java
+++ src/main/java/vector/StockManagement/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
                                 .requestMatchers("/api/auth/register").hasRole("ADMIN")
                                 .requestMatchers("api/tenants/admin").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/users/me").authenticated()
-                                .requestMatchers(HttpMethod.GET, "/api/users").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/users").hasAnyRole("ADMIN", "SUPER_ADMIN")
                                 .requestMatchers(HttpMethod.POST, "/api/tenants/***").hasRole("SUPER_ADMIN")
                                 .requestMatchers("/api/management/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_MANAGER")
                                 .requestMatchers(HttpMethod.GET, "/api/management/**").hasAnyRole("ADMIN_READ", "MANAGER_READ")
