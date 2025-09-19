15d21a2eef890b7f7ba2f5273bffc710b9de8a31
vieira-klgwn <ntwavieir7@gmail.com>
2025-09-19 08:37:39 +0200

    made pricelist item fields required

--- src/main/java/vector/StockManagement/model/dto/PriceListItemDTO.java
+++ src/main/java/vector/StockManagement/model/dto/PriceListItemDTO.java
@@ -1,13 +1,18 @@
 package vector.StockManagement.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 
 @Data
 public class PriceListItemDTO {
+    @NotBlank
     private Long priceListId;
+    @NotBlank
     private Long productId;
+    @NotBlank
     private Long tenantId;
+    @NotBlank
     private Long basePrice;
     private Long minPrice;
 }
